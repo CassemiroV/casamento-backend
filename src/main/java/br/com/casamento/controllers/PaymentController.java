1c6da35587257b8a84edfe630976be70f9bd4b34
Vinicius <viniciuscasse11@gmail.com>
2026-09-24 23:20:52 -0300

    Add CORS

--- src/main/java/br/com/casamento/controllers/PaymentController.java
+++ src/main/java/br/com/casamento/controllers/PaymentController.java
@@ -2,9 +2,9 @@ package br.com.casamento.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.casamento.dtos.request.CreatePaymentRequest;
 import br.com.casamento.dtos.response.PaymentResponse;
@@ -17,7 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @CrossOrigin(origins = {
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "http://viniciuscassemiro.com.br",
+    "https://viniciuscassemiro.com.br",
+    "https://www.viniciuscassemiro.com.br"
 })
 public class PaymentController {
     private final PaymentService paymentService;
